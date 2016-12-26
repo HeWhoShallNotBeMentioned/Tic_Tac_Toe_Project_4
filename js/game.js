41bb178c945d534543ed982f9ac5bfca0974b1e8
Chris Underwood <cunderwoodmn@gmail.com>
2016-12-25 22:34:15 -0700

    Add code to find a winner

--- js/game.js
+++ js/game.js
@@ -6,6 +6,7 @@ this.spaces = [];
 
 document.getElementsByClassName("players")[1].classList.add("active");
   var boardArray = [-1,-1,-1,-1,-1,-1,-1,-1,-1];
+  var symbol = this.symbol;
  var game = this;
  var doTurn = function() {
    if (game.currentPlayer == playerOne ) {
@@ -21,12 +22,34 @@ document.getElementsByClassName("players")[1].classList.add("active");
 };
 
 var win = function() {
+  var result = [];
+  var curPlayBoxes = [];
+  var winningCombos = [[0, 1, 2], [3, 4, 5,], [6, 7, 8], [0, 3, 6], [1, 4 ,7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
   console.log("inside win");
   console.log("this.position:   ", this.position);
   console.log("this.symbol:  ", this.symbol);
   console.log("boardArray:  ", boardArray);
+
   boardArray.splice(this.position,1,this.symbol);
   console.log("boardArray:  ", boardArray);
+
+  for (var i = 0; i < boardArray.length; i++) {
+    if (this.symbol == boardArray[i]) {
+      curPlayBoxes.push(i);
+    }
+  }
+  console.log("Current Player Boxes:  ", curPlayBoxes);
+  winningCombos.forEach(function(combo) {
+    for (var i = 0; i <3; i++) {
+      result[i] = (curPlayBoxes).includes(combo[i]); //gets results for each winning combination
+    }
+    if ((result[0] === true) && (result[1] === true) && (result[2] === true)) {
+       console.log("current player wins:  ", game.currentPlayer.symbol);
+       //console.log("current player", currentPlayer);
+    }
+  });
+
+
 };
 
  for(var i = 0; i < 9; i++){
